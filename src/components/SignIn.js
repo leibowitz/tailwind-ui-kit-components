923c5ce45961f8be15aa09fcb7b0ab9a713dc097
Gianni Moschini <gianni.moschini+github@gmail.com>
2021-07-27 18:07:48 +0100

    re-use NavItemLink in Sign In

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,20 +1,10 @@
-import tw from 'twin.macro';
-import { useContext } from 'react';
-
-import DarkContext from './Context';
+import NavItemLink from './NavItemLink';
 
 const SignIn = () => {
-  const dark = useContext(DarkContext);
-
   return (
-    <a
-      href="/"
-      aria-label="Sign in"
-      title="Sign in"
-      css={[tw`font-medium tracking-wide transition-colors duration-200 no-underline`, dark ? tw`text-gray-100 hover:text-teal-accent-400` : tw`text-gray-700 hover:text-deep-purple-accent-400`]}
-    >
+    <NavItemLink title="Sign In">
       Sign in
-    </a>
+    </NavItemLink>
   );
 };
 
