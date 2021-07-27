8f34ae43cdb4ccb083587347764fe02920db0b47
Gianni Moschini <gianni.moschini+github@gmail.com>
2021-07-27 23:12:48 +0100

    fix CallToAction hover bg color

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -9,7 +9,7 @@ const CallToAction = () => {
     <div>
       <a
         href="/"
-        css={[tw`inline-flex items-center justify-center h-12 px-6 tracking-wide transition duration-200 rounded shadow-md focus:shadow-outline focus:outline-none`, accent ? tw`font-semibold text-teal-900 hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-deep-purple-accent-100` : tw`text-white font-medium bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700`]}
+        css={[tw`inline-flex items-center justify-center h-12 px-6 tracking-wide transition duration-200 rounded shadow-md focus:shadow-outline focus:outline-none`, accent ? tw`font-semibold text-teal-900 hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-teal-accent-700` : tw`text-white font-medium bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700`]}
       >
         Get started
       </a>
