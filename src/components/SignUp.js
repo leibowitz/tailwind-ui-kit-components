be5aabd32702547ce9c322cf4826c287fbd3edcb
Gianni Moschini <gianni.moschini+github@gmail.com>
2021-07-27 18:07:38 +0100

    add option to show Sign Up as text

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,26 @@
 import tw from 'twin.macro';
 
-const SignUp = ({ large }) => (
-  <a
-    href="/"
-    css={[tw`inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none no-underline`, large && tw`w-full`]}
-    aria-label="Sign up"
-    title="Sign up"
-  >
-    Sign up
-  </a>
-);
+import NavItemLink from './NavItemLink';
+
+const SignUp = ({ large, text }) => {
+  if (text) {
+    return (
+      <NavItemLink title="Sign up">
+        Sign Up
+      </NavItemLink>
+    );
+  }
+
+  return (
+    <a
+      href="/"
+      css={[tw`inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none no-underline`, large && tw`w-full`]}
+      aria-label="Sign up"
+      title="Sign up"
+    >
+      Sign up
+    </a>
+  );
+};
 
 export default SignUp;
