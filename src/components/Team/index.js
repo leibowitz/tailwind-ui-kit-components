603b10622e725e9001d707f2b0a8b8544292f258
Gianni Moschini <gianni.moschini+github@gmail.com>
2021-08-02 18:58:33 +0100

    add title and intro to Team component

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -27,9 +27,15 @@ Person.propTypes = {
   }),
 };
 
-const Team = ({ peoples }) => {
+const Team = ({ peoples, title, intro }) => {
   return (
     <div tw="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div tw="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <h2 tw="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          {title}
+        </h2>
+        <p tw="text-base text-gray-700 md:text-lg">{intro}</p>
+      </div>
       <div tw="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
         {peoples.map((person, index) => (
           <Person key={index} {...person} />
@@ -40,6 +46,8 @@ const Team = ({ peoples }) => {
 };
 
 Team.propTypes = {
+  title: PropTypes.string,
+  intro: PropTypes.string,
   peoples: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,
